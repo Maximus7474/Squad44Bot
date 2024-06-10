0278e874c9a9dff15b16eee9b4f38955622bc348
Maximus7474 <the.maximus.7474@gmail.com>
2024-06-10 18:35:40 +0200

    feat(tests): check the discord token & env variables

--- test/config.mjs
+++ test/config.mjs
@@ -3,7 +3,7 @@ import * as chai from 'chai';
 
 const { expect } = chai;
 
-describe('Config Files Test', () => {
+describe('Configuration Test', () => {
 
     it('should check if config.json exists', () => {
         
@@ -19,6 +19,27 @@ describe('Config Files Test', () => {
         const fileExists = fs.existsSync(configFile);
 
         expect(fileExists).to.be.true;
+    });
+    
+    it('should check if a token exists', () => {
+        const configFile = './.env';
+        const fileExists = fs.existsSync(configFile);
+        
+        if (!fileExists) {
+            throw new Error('The .env file does not exist.');
+        }
+    
+        const envContent = fs.readFileSync(configFile, 'utf8');
+        const tokenValue = envContent.split('\n').find(line => line.startsWith('TOKEN='));
+        const token = tokenValue.split('=')[1];
+        
+        if (!tokenValue || token === 'INSERT_YOUR_TOKEN_HERE') {
+            throw new Error('The token is not defined in the .env file.');
+        }
+        
+        if (token.length < 70 || !token.includes('.') || (token.match(/\./g) || []).length !== 2) {
+            throw new Error('The token is not valid.');
+        }
     });
 
 });
