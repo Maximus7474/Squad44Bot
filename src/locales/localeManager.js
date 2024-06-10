fb4032446cbae1b400b7d64fe33062a2ac236be0
Maximus7474 <the.maximus.7474@gmail.com>
2024-06-10 16:21:21 +0200

    rf(localeManager): console logging

--- src/locales/localeManager.js
+++ src/locales/localeManager.js
@@ -8,7 +8,7 @@ const language = configManager.getConfig().language
 try {
     translations = require(`./${language}.json`);
 } catch (err) {
-    logger.error(`Setup language: '${language}' doensn't exist in the locales folder. Create the translation in './src/locales/'`)
+    if (language !== undefined) logger.error(`Setup language: '${language}' doensn't exist in the locales folder. Create the translation in './src/locales/'`);
     logger.info('Defaulting to English language')
 
     translations = require(`./en.json`);
