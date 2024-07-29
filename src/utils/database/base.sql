dea9e8be550f6e3d3130ddabb0563b946a370679
Maximus7474 <the.maximus.7474@gmail.com>
2024-07-29 20:34:50 +0200

    rf(sql)!!: structure

--- src/utils/database/base.sql
+++ src/utils/database/base.sql
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS "user-preferences" (
 CREATE TABLE IF NOT EXISTS "game-clans" (
   "id" INTEGER PRIMARY KEY AUTOINCREMENT,
   "tag" TEXT UNIQUE NOT NULL,
-  "fullName" TEXT NOT NULL,
-  "serverInvite" TEXT DEFAULT NULL,
+  "name" TEXT NOT NULL,
+  "invite" TEXT DEFAULT NULL,
   "otherLinks" TEXT DEFAULT '[]',
   "description" TEXT DEFAULT NULL,
   "seal" TEXT DEFAULT NULL,
