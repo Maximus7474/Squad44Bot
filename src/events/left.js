25321df929ab6d79c37d2c16f043538fb02b6bbb
Maximus7474 <the.maximus.7474@gmail.com>
2024-07-30 20:19:35 +0200

    feat: join & leave event handling

---
+++ src/events/left.js
@@ -0,0 +1,34 @@
+const { Events, EmbedBuilder } = require('discord.js');
+
+const configManager = require('../utils/configManager.js');
+
+module.exports = {
+    event: Events.GuildMemberAdd,
+    type: "on",
+    async call(client, member, ...args) {
+        
+        const { channels, main_guild } = configManager.getConfig();
+
+        if (!main_guild === member.guild.id) return;
+
+        if (channels.joins.id === null) return;
+
+        const channel = client.channels.cache.get(channels.joins.id);
+        if (channel) {
+            const leaveEmbed = new EmbedBuilder()
+                .setTitle('User Left')
+                .setColor('Orange')
+                .setDescription(`- Username: ${member.user.tag}\n- ID: ${member.user.id}\n- Joined at: <t:${Math.round(member.user.joinedTimestamp/1000)}:d>`);
+            
+            try {
+                leaveEmbed.setThumbnail(member.user.displayAvatarURL({ dynamic: true, format: 'png', size: 256 }));
+            } catch (error) {
+                leaveEmbed.setThumbnail(client.user.displayAvatarURL({ dynamic: true, format: 'png', size: 256 }));
+            }
+            
+                channel.send({
+                    embeds: [leaveEmbed]
+                });
+        }
+    }
+}
