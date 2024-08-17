31ca9c6389c4cf6595ce8b2448f6a9c7d2fc7804
Maximus7474 <the.maximus.7474@gmail.com>
2024-08-17 23:21:47 +0200

    feat: faction viewer  command -> from: 9d8934d107f53873c7901c1f92d7f1cce5410513

---
+++ src/commands/factionViewer.js
@@ -0,0 +1,107 @@
+const { EmbedBuilder, AttachmentBuilder, SlashCommandBuilder } = require('discord.js');
+
+const { Allies, Axis } = require('../../data/factions.json');
+
+const getFileName = (filepath) => {
+    if (filepath && filepath.includes('/')) {
+        return filepath.split('/').pop();
+    }
+    return filepath;
+}
+
+module.exports = {
+    register_command: new SlashCommandBuilder()
+        .setName('faction')
+        .setDescription('Displays information on a specific faction')
+        .addSubcommand( subcommand =>
+            subcommand
+            .setName("axis")
+            .setDescription("Search for a faction part of the axis forces")
+            .addStringOption(option => 
+                option.setName('faction')
+                .setDescription("Faction Name")
+                .setRequired(true)
+                .addChoices(
+                    ...Object.keys(Axis).map(faction => ({
+                        name: faction,
+                        value: faction
+                    }))
+                )
+            )
+        )
+        .addSubcommand( subcommand =>
+            subcommand
+            .setName("allies")
+            .setDescription("Search for a faction part of the allied forces")
+            .addStringOption(option => 
+                option.setName('faction')
+                .setDescription("Faction Name")
+                .setRequired(true)
+                .addChoices(
+                    ...Object.keys(Allies).map(faction => ({
+                        name: faction,
+                        value: faction
+                    }))
+                )
+            )
+        ),
+    async execute(client, interaction) {
+        const subcommand = interaction.options._subcommand;
+        const faction = interaction.options.getString('faction');
+
+        let factionData;
+        if (subcommand === 'axis') factionData = Axis[faction];
+        if (subcommand === 'allies') factionData = Allies[faction];
+
+        if (!faction) return interaction.reply({
+            content: `No faction data is available for ${faction} at the moment.`,
+            ephemeral: true
+        })
+
+        let attachments = [];
+
+        const factionEmbed = new EmbedBuilder()
+            .setTitle(faction)
+            .setDescription(factionData.description || null)
+            .setColor(subcommand === 'axis' ? 14160916 : 3163276);
+
+        if (typeof factionData.seal === 'string') {
+            if (factionData.seal.includes('images/seals/')) {
+                const attachmentFile = new AttachmentBuilder(`./${factionData.seal}`, {name: getFileName(factionData.seal), description: `The seal used by ${faction}`})
+
+                factionEmbed.setThumbnail(`attachment://${getFileName(factionData.seal)}`)
+                attachments.push(attachmentFile)
+            } else {
+                factionEmbed.setThumbnail(factionData.seal)
+            }
+        } else {
+            factionEmbed.setThumbnail(client.user.displayAvatarURL({ dynamic: true, form: 'png', size: 128}));
+        }
+
+        if (typeof factionData.uniform === 'string') {
+            if (factionData.uniform.includes('images/uniforms/')) {
+                const attachmentFile = new AttachmentBuilder(`./${factionData.uniform}`, {name: getFileName(factionData.uniform), description: `The uniforms used by ${faction}`});
+                
+                factionEmbed.setImage(`attachment://${attachmentFile.name}`)
+                attachments.push(attachmentFile)
+            } else {
+                factionEmbed.setImage(factionData.uniform)
+            }
+        }
+
+        if (factionData.links instanceof Array) {
+            factionEmbed.addFields(
+                { name: "__Links__", value: factionData.links.map(item => `- [${item.name}](${item.link})`).join('\n')}
+            );
+        }
+
+        factionEmbed.setFooter({text: "This data here can be inacurate, to contribute check out my bio."})
+
+        return interaction.reply({
+            content: ``,
+            ephemeral: false,
+            embeds: [factionEmbed],
+            files: attachments
+        });
+    }
+}
