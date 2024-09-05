c0bca1ccd6764b2a7bccc42e851e0d2d7053bed2
Maximus7474 <the.maximus.7474@gmail.com>
2024-09-05 22:32:29 +0200

    feat(serverStatus): added recurring task to generate status messages

--- src/commands/serverStatus.js
+++ src/commands/serverStatus.js
@@ -1,8 +1,9 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { PermissionsBitField, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+const { PermissionsBitField, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 const { executeStatement, executeQuery } = require('../utils/database/sqliteHandler');
 const { queryBattleMetrics } = require('../utils/query/battlemetricsQuery');
+const { emojiForStatus, capitalizeFirstLetter, getOrdinalSuffix } = require('../utils/functions');
 
 const log = new require('../utils/logger.js');
 const logger = new log("Server Status");
@@ -27,7 +28,7 @@ const extractBMId = (UrlOrId) => {
     }
 
     return null;
-}
+};
 
 const setStatusBreakdown = (guildId, channelId, bmId) => {
     if (!statusBreakdown[guildId]) {
@@ -40,6 +41,45 @@ const setStatusBreakdown = (guildId, channelId, bmId) => {
 const howManyObservedServers = (channelBreakdown) => {
     const uniqueServers = new Set(Object.values(channelBreakdown));
     return uniqueServers.size;
+};
+
+const listBMids = () => {
+    let newTable = {};
+
+    for (let outerKey in statusBreakdown) {
+        for (let innerKey in statusBreakdown[outerKey]) {
+            newTable[statusBreakdown[outerKey][innerKey]] = {};
+        }
+    }
+
+    return newTable;
+}
+
+const serverEmbedAndButtons = (bmid, data) => {
+    const Embed = new EmbedBuilder()
+    .setTitle(data.attributes.name)
+    .setColor(16316405)
+    .setDescription(
+       `**__Status:__ ${emojiForStatus(data.attributes.status)} ${capitalizeFirstLetter(data.attributes.status)}**`
+    )
+    .addFields(
+        { name: "Rank:", value: getOrdinalSuffix(data.attributes.rank), inline: true},
+        { name: "Players:", value: `${data.attributes.players}${data.attributes.details.squad_publicQueue > 0 ? `(+${data.attributes.details.squad_publicQueue + data.attributes.details.squad_reservedQueue})` : ""}/${data.attributes.maxPlayers}`, inline: true },
+        { name: "Layer:", value: data.attributes.details.map, inline: true }
+    );
+
+    const BMLink = new ButtonBuilder()
+        .setLabel('Battlemetrics page')
+        .setStyle(ButtonStyle.Link)
+        .setURL(`https://www.battlemetrics.com/servers/postscriptum/${bmid}`);
+
+    const row = new ActionRowBuilder()
+        .addComponents(BMLink);
+
+    return {
+        embeds: [Embed],
+        components: [row]
+    }
 }
 
 module.exports = {
@@ -58,6 +98,76 @@ module.exports = {
                     option.setName('server').setDescription('The battlemetrics ID or link of the server').setRequired(true)
                 )
         ),
+    async setup_command(client) {
+        const DBbreakdown = await executeQuery('SELECT `guild`, `channels` FROM `server-status-channels`;', [], 'all');
+
+        DBbreakdown.forEach(element => {
+            let { guild, channels } = element;
+            channels = JSON.parse(channels);
+
+            for (const [channel_id, bmID] of Object.entries(channels)) {
+                setStatusBreakdown(guild, channel_id, bmID);
+            }
+        });
+
+        setInterval(async () => {
+            const queryList = listBMids();
+        
+            const promises = Object.keys(queryList).map(async (identifier) => {
+                try {
+                    let data = await queryBattleMetrics(`servers/${identifier}`);
+                    data = data.data;
+        
+                    if (data.attributes.name.includes('discord')) {
+                        const index = data.attributes.name.toLowerCase().indexOf('discord');
+                        data.attributes.name = index !== -1 ? data.attributes.name.substring(0, index).trim() : data.attributes.name;
+                    } else if (data.attributes.name.includes('gg/')) {
+                        const index = data.attributes.name.toLowerCase().indexOf('gg/');
+                        data.attributes.name = index !== -1 ? data.attributes.name.substring(0, index).trim() : data.attributes.name;
+                    }
+        
+                    queryList[identifier] = serverEmbedAndButtons(identifier, data);
+                } catch (err) {
+                    logger.error(`Error processing ${identifier}:`, err);
+                }
+            });
+        
+            await Promise.all(promises);
+
+            Object.keys(statusBreakdown).forEach((guild_id) => {
+                const guild = client.guilds.cache.get(guild_id);
+                if (!guild) {
+                    logger.error('Guild not found:', guild_id);
+                    return;
+                }
+            
+                Object.keys(statusBreakdown[guild_id]).forEach((channel_id) => {
+                    const channel = guild.channels.cache.get(channel_id);
+            
+                    if (!channel || !channel.isTextBased()) {
+                        logger.error('Channel not found or is not a text-based channel:', guild.name, guild_id, channel_id);
+                        return;
+                    }
+            
+                    channel.messages.fetch({ limit: 100 })
+                        .then(messages => {
+                            const botMessages = messages.filter(msg => msg.author.id === client.user.id);
+                            
+                            if (botMessages.size > 0) {
+                                const lastBotMessage = botMessages.first();
+                                lastBotMessage.edit(queryList[statusBreakdown[guild_id][channel_id]]);
+                            } else {
+                                channel.send(queryList[statusBreakdown[guild_id][channel_id]]);
+                            }
+                        })
+                        .catch(err => {
+                            logger.error('Unable to handle the message send for', guild.name, guild_id, channel_id, err);
+                        });
+                });
+            });   
+        
+        }, 1000 * 10);
+    },
     async execute(client, interaction) {
         const subcommand = interaction.options._subcommand;
         const { user, guild } = interaction;
