0ab97d01b549d072aa099f31cf56274f20ca6440
Maximus7474 <the.maximus.7474@gmail.com>
2024-09-06 17:55:30 +0200

    chore: validate lint test

--- src/commands/serverStatusHandler.js
+++ src/commands/serverStatusHandler.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { PermissionsBitField, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+const { PermissionsBitField, EmbedBuilder } = require('discord.js');
 
 const { executeStatement, executeQuery } = require('../utils/database/sqliteHandler');
 
